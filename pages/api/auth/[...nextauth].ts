23d0d2815a06454afb7616137777871a69f71a74
MohamedBoukthir <medboukthir7@gmail.com>
2023-11-09 16:23:00 +0100

    NextAuth

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,65 @@
+import NextAuth from "next-auth"
+import bcrypt from 'bcrypt'
+import GoogleProvider from "next-auth/providers/google"
+import CredentialsProvider from "next-auth/providers/credentials"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+
+import prisma from '@/libs/prismadb'
+
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    CredentialsProvider({
+        name: 'credentials',
+        credentials: {
+            email: {
+                label: 'email',
+                type: 'text',
+            },
+            password: {
+                label: 'password',
+                type:'password'
+            },
+        },
+        async authorize(credentials) {
+
+            if(!credentials?.email || !credentials.password){
+                throw new Error('INVALID EMAIL OR PASSWORD')
+            }
+
+            const user = await prisma.user.findUnique({
+                where: {
+                    email: credentials.email
+                }
+            })
+
+            if(!user || user?.hashedPassword){
+                throw new Error('INVALID EMAIL OR PASSWORD')
+            }
+
+            const isCorectPassword = await bcrypt.compare(
+                 credentials.password,
+                 user.hashedPassword
+            )
+
+            if(!isCorectPassword){
+                throw new Error('INVALID EMAIL OR PASSWORD')
+            }
+
+            return user;
+        }
+    }),
+  ],
+  pages: {
+    signIn: '/login',
+  },
+  debug: process.env.NODE_ENV === 'development',
+  session: {
+    strategy: 'jwt',
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+});
